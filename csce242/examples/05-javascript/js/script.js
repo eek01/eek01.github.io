82bc067a8c7161cf97b29580030b9d3ef350ed7c
erink0415 <erinekelly0415@yahoo.com>
2026-02-10 09:41:39 -0500

    new javascript ex

--- csce242/examples/05-javascript/js/script.js
+++ csce242/examples/05-javascript/js/script.js
@@ -10,3 +10,37 @@ document.getElementById("a-click").onclick = (e) => {
     e.preventDefault(); // dont go to the links destination (to not have the # at the end of the http)
     e.currentTarget.innerHTML = "Clicked";
 };
+
+//start and stop ball bouncing
+document.getElementById("btn-bounce").onclick = (e) => {
+    const ball = document.getElementById("ball");
+
+    if(e.currentTarget.innerHTML.toLowerCase() == "start") {
+        e.currentTarget.innerHTML = "Stop";
+        ball.classList.add("bounce");
+    } else {
+        e.currentTarget.innerHTML = "Start";
+        ball.classList.remove("bounce");
+    }
+}
+
+document.getElementById("txt-num-days").onchange = (e) => {
+    const numEntered = parseInt(e.currentTarget.value);
+    const p = document.getElementById("p-plant-msg");    
+
+    if(numEntered <=1) {
+        p.innerHTML = "Yay we were fed today!";
+    } else if(numEntered <=2) {
+        p.innerHTML = "Im getting a lil thirstyyy"
+    } else if (numEntered <= 5) {
+        p.innerHTML = "Im starting to wilttttt"
+    } else {
+        p.innerHTML = "you killed meeee X("
+    }
+    
+};
+
+//toggle navvvv
+document.getElementById("toggle-nav").onclick = () => {
+    document.querySelector("#main-nav ul").classList.toggle("hide-small");
+};
