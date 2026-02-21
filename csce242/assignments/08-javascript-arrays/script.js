115eb09b077f3566d7bb2ed76c95b60237085fa3
erink0415 <erinekelly0415@yahoo.com>
2026-02-20 21:48:37 -0500

    final debug?

--- csce242/assignments/08-javascript-arrays/script.js
+++ csce242/assignments/08-javascript-arrays/script.js
@@ -1,16 +1,16 @@
 const happySongs = [];
-happySongs["Happy by Pharrell Williams"] = "WDw7mqHHgl22iAlJ";
-happySongs["Don't Stop Me Now by Queen"] = "Tg7Amg-XOpdnQ-Qf";
-happySongs["Can't Stop This Feeling by Justin Timberlake"] = "jpOgJOek6xbidUcS";
-happySongs["Don't Worry Be Happy by Bobby McFerrin"] = "yaNsLe4Gjvh8GNp6";
-happySongs["I'm Walking on Sunshine by Katrine and the Waves"] = "gI5as1yeR9Lwsvww";
+happySongs["Happy by Pharrell Williams"] = "ZbZSe6N_BXs?si=WDw7mqHHgl22iAlJ";
+happySongs["Don't Stop Me Now by Queen"] = "HgzGwKwLmgM?si=Tg7Amg-XOpdnQ-Qf";
+happySongs["Can't Stop This Feeling by Justin Timberlake"] = "ru0K8uYEZWw?si=jpOgJOek6xbidUcS";
+happySongs["Don't Worry Be Happy by Bobby McFerrin"] = "d-diB65scQU?si=yaNsLe4Gjvh8GNp6";
+happySongs["I'm Walking on Sunshine by Katrine and the Waves"] = "iPUmE-tne5U?si=gI5as1yeR9Lwsvww";
 
 const sadSongs = [];
-sadSongs["Happier Then Ever by Billie Eillish"] = "ZFtTh8T3ZpgZkDkY";
-sadSongs["Someone You Loved by Lewis Capaldi"] = "joU3CQwhaUCV2BXC";
-sadSongs["Someone Like You by Adele"] = "Ec-74zqqvOqpayiw";
-sadSongs["Fix You by Coldplay"] = "TLouELYQ6RatbJik";
-sadSongs["Hurt by Johnny Cash"] = "U3FP7k9pMAYoip9y";
+sadSongs["Happier Then Ever by Billie Eillish"] = "5GJWxDKyk3A?si=ZFtTh8T3ZpgZkDkY";
+sadSongs["Someone You Loved by Lewis Capaldi"] = "zABLecsR5UE?si=joU3CQwhaUCV2BXC";
+sadSongs["Someone Like You by Adele"] = "hLQl3WQQoQ0?si=Ec-74zqqvOqpayiw";
+sadSongs["Fix You by Coldplay"] = "k4V3Mo61fJM?si=TLouELYQ6RatbJik";
+sadSongs["Hurt by Johnny Cash"] = "8AHCfZTRGiI?si=U3FP7k9pMAYoip9y";
 
 const dropBtn = document.getElementById("dropbtn");
 const ytFrame = document.getElementById("yt-frame");
