f5eaf9687d7f5648293f02a282f2087e906cba07
erink0415 <erinekelly0415@yahoo.com>
2026-02-28 10:16:35 -0500

    finished logic for class

--- csce242/assignments/09-javascript-classes/script.js
+++ csce242/assignments/09-javascript-classes/script.js
@@ -34,12 +34,17 @@ class Song {
         //adding to page
         songDiv.append(a1);
 
-        songContainer.append(this.modal());
-
         a1.onclick = (e) => {
             e.preventDefault();
-            document.getElementById('id01').style.display='block';
-            console.log("clicked!!");
+            const modal = this.modal();
+            songContainer.append(modal);
+
+            modal.style.display='block';
+
+            document.querySelector(".w3-container").onclick = () => {
+                modal.style.display='none';
+                modal.remove();
+            };
         };
         return songContainer;
     }
@@ -77,12 +82,12 @@ class Song {
 
         columnsDiv.append(this.modalInfo());
 
-        console.log("added modal!!");
         return modalDiv;
     }
 
     modalInfo() {
         const div = document.createElement("div");
+        div.classList.add("modal-txt");
         
         const modalTitle = document.createElement("h3");
         modalTitle.innerHTML = this.title;
@@ -103,26 +108,24 @@ class Song {
     }
     
     setAttributes(frame) {
-        frame.setAttribute("src", `www.youtube.com/embed/${this.ytCode}`);
+        frame.setAttribute("src", "https://www.youtube.com/embed/"+this.ytCode);
+        frame.setAttribute("title", "YouTube video player");
+        frame.setAttribute("frameborder", "0");
+        frame.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+        frame.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
         frame.setAttribute("allowfullscreen", true);
     }
 }
 
-
-
 const songs = [];
 songs.push(new Song("Sailboat", "Cavetown", "Running With Scissors", 2026, "Bedroom Pop", "images/sailboat-cover.jpg", "KaHvNbId7BY?si=9y09EVPxGapnHjfw"));
-songs.push(new Song("Kiss Me Again", "Babe City", "I Love You Forever", 1111, "Pop", "images/kiss-me-again-cover.png", "n4VxOz7faFk?si=jhqsLvpJiOFeEjpz"));
+songs.push(new Song("Kiss Me Again", "Babe City", "I Love You Forever", 2024, "Pop", "images/kiss-me-again-cover.png", "n4VxOz7faFk?si=jhqsLvpJiOFeEjpz"));
 songs.push(new Song("Stateside + Zara Larsson", "PinkPantheress", "Fancy Some More?", 2025, "Pop", "images/stateside-cover.png", "lIxQe1R5hs0?si=eT74f9iHHQR5NjwH"))
-songs.push(new Song("Jackie and Wilson", "Hozier", "Hozier (Expanded Edition)", 2014, "Soulful", "images/jackie-and-wilson-cover.png", "cSWqxbswQAY?si=XxlJlyZRGksd3Lyb"));
+songs.push(new Song("Jackie and Wilson", "Hozier", "Hozier (Expanded Edition)", 2014, "Soul", "images/jackie-and-wilson-cover.png", "cSWqxbswQAY?si=XxlJlyZRGksd3Lyb"));
 
 songs.forEach((song)=>{
     document.getElementById("music").append(song.item);
 });
-
-document.querySelector(".w3-container span").onclick = () => {
-    document.getElementById('id01').style.display='none';
-};
 
 // sailboat by cavetown:                       <iframe src="https://www.youtube.com/embed/KaHvNbId7BY?si=9y09EVPxGapnHjfw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 // kiss me again by babecity:                  <iframe src="https://www.youtube.com/embed/n4VxOz7faFk?si=jhqsLvpJiOFeEjpz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
