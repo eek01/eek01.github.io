db3b6290c37b4016bba834a865d224eed833e894
erink0415 <erinekelly0415@yahoo.com>
2026-03-05 17:33:56 -0500

    added photos to json

--- csce242/project/part7-JSON-Contact Form/js/jsons.js
+++ csce242/project/part7-JSON-Contact Form/js/jsons.js
@@ -11,14 +11,32 @@ const showWomens = async() => {
     const womens = await getWomens();
     const womensDiv = document.getElementById("womens-shop");
 
-    womens.forEach(women)=>{
+    console.log("hiii");
+    womens.forEach((woman)=>{
         const a = document.createElement("a");
+        a.href=`${woman.link}`
         womensDiv.prepend(a);
 
         const section = document.createElement("section");
         section.classList.add("shop-section");
         a.append(section);
 
+        const img = document.createElement("img");
+        section.append(img);
+        img.src=`https://eek01.github.io/csce242/json/images/womens-images/${woman.img_name}`;
 
-    };
+        const div = document.createElement("div");
+        div.classList.add("shop-section-txt");
+        section.append(div);
+
+        const name = document.createElement("p");
+        name.innerHTML = `${woman.title}`;
+        div.append(name);
+
+        const price = document.createElement("p");
+
+
+    });
 };
+
+showWomens();
